504533c91f4fb55d428f9fb97ea15d67db87dbe6
shubham-qw <shubhamgoel12345678@gmail.com>
2023-01-19 11:54:55 +0530

    Couch Potato Changes

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongooseURI = "mongodb://Shubham:9013684040@ac-d3btdft-shard-00-00.4w5xiol.mongodb.net:27017,ac-d3btdft-shard-00-01.4w5xiol.mongodb.net:27017,ac-d3btdft-shard-00-02.4w5xiol.mongodb.net:27017/CouchPotato?ssl=true&replicaSet=atlas-3222at-shard-0&authSource=admin&retryWrites=true&w=majority";
+const mongooseURI = require("./models/pass");
 
 const Data = [];
 mongoose.set('strictQuery', false);
